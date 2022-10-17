4a87185c41971152aa5af2cb1777fe4024f60104
Ivan Zharikov <izharikov888@gmail.com>
2022-10-17 15:28:02 -0400

    Initial cookie

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,15 @@
+const img = document.getElementById("cookie");
+const counter = document.getElementById("clicker__counter");
+
+
+img.onclick = function() {
+    counter.textContent = Number(counter.textContent) + 1;
+    if (cookie.width == 150) {
+        cookie.width = 200}
+    else {cookie.width = 150
+    }}
+
+
+
+
+
