0b3880001638f12ae16e51a75da7cb179c16879f
Ivan Zharikov <izharikov888@gmail.com>
2022-10-18 16:35:04 -0400

    initial popup

---
+++ element-search/popups/task.js
@@ -0,0 +1,15 @@
+document.getElementById("modal_main").className = "modal modal_active";
+
+const button_good = document.querySelector(".show-success");
+const button_close = document.querySelectorAll("div.modal__close");
+
+button_good.onclick = function() {
+    document.getElementById("modal_success").className = "modal modal_active";
+}
+
+for (idx = 0; idx < button_close.length; idx++) {
+    let el = button_close.item(idx);
+    el.onclick = function() {
+        document.getElementById("modal_main").className = "modal";
+        document.getElementById("modal_success").className = "modal";
+}}
