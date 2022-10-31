58d548e386c7cf9a365eace3a29dba8052ba91e4
Ivan Zharikov <izharikov888@gmail.com>
2022-10-31 13:53:21 -0400

    IZ third

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,16 +1,16 @@
 const menuLink = Array.from(document.querySelectorAll('.menu__link'));
+const menuItem = Array.from(document.querySelectorAll('.menu__item'));
 
-for (let idx = 0; idx <= menuLink.length; idx++) {
+for (let idx = 0; idx < menuLink.length; idx++) {
 	menuLink[idx].onclick = function () {
-		const parent = menuLink[idx].closest('.menu__item');
-		if (parent.querySelector('.menu_sub').className === 'menu menu_sub') {
-			parent.querySelector('.menu_sub').className = 'menu menu_sub menu_active';
-		} else {
-			parent.querySelector('.menu_sub').className = 'menu menu_sub'
-		}
-
-		if (menuLink[idx].closest('.menu_main')) {
-			return false
+		if (menuItem[idx].querySelector('.menu_sub') !== null && menuItem[idx].querySelector('.menu_active') === null) {
+			for (let i = 0; i < menuItem; i++) {
+				if (menuItem[i].querySelector('.menu_active')) {
+					menuItem[i].querySelector('.menu_sub').className = 'menu menu_sub'
+				}
+			}
+		    menuItem[idx].querySelector('.menu_sub').className = ('menu menu_sub menu_active');
+            return menuItem[idx].href = false;
 		}
 	}
 }
