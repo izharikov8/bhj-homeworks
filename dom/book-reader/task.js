8f14c3dbe7e0421122768c833944874c0b0b43e0
Ivan Zharikov <izharikov888@gmail.com>
2022-11-07 15:17:12 -0500

    Book-reader-2.1

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,4 +1,5 @@
 const bookBtn = document.querySelector('.book');
+const fonts = Array.from(document.querySelectorAll('.font-size'))
 
 bookBtn.onclick = function(event) {
     for (var idx = 0; idx < fonts.length; idx++) {
