160556008e9eae30e038b8d632ceba315e51b1c3
Ivan Zharikov <izharikov888@gmail.com>
2022-11-07 15:12:37 -0500

    Book-reader

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,13 +1,22 @@
 const bookBtn = document.querySelector('.book');
+const fonts = Array.from(document.querySelectorAll('.font-size'))
 
 bookBtn.onclick = function(event) {
-    if (event.target.className === 'font-size font-size_small') {
-        bookBtn.className = 'book book_fs-small'
+    for (var idx = 0; idx < fonts.length; idx++) {
+        if (fonts[idx].classList.contains('font-size_active')){
+            fonts[idx].classList.remove('font-size_active')
+        }
     }
-    if (event.target.className === 'font-size font-size_active') {
-        bookBtn.className = 'book'
+    if (event.target.className === 'font-size font-size_small') {
+        event.target.className = 'font-size font-size_small font-size_active';
+        bookBtn.className = 'book book_fs-small';
+    }
+    if (event.target.className === 'font-size' ) {
+        event.target.className = 'font-size font-size_active';
+        bookBtn.className = 'book';
     }
     if (event.target.className === 'font-size font-size_big') {
+        event.target.className = 'font-size font-size_big font-size_active'    
         bookBtn.className = 'book book_fs-big'
     }
     event.preventDefault()
